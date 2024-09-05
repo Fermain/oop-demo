6bf1d91018468505b07a3b8b0a236fe2e058f7d4
Fermain <Fermain@users.noreply.github.com>
2024-09-05 13:55:18 +0200

    Adds view layer to router

--- js/ui/index.js
+++ js/ui/index.js
@@ -2,6 +2,48 @@ import NoroffAPI from "../api/index.js";
 
 export default class NoroffApp extends NoroffAPI {
 
+  constructor() {
+    super()
+    this.route()
+  }
+
+  async route(href = window.location.href) {
+    const url = new URL(href);
+    const params = Object.fromEntries(url.searchParams.entries())
+    const pathname = url.pathname
+
+    switch (pathname) {
+      case "/":
+      case "/index.html":
+        await this.views.home()
+        break;
+      case "/auth/register.html":
+        await this.views.register()
+        break;
+      case "/auth/login.html":
+        await this.views.login()
+        break;
+      case "/post/index.html":
+      case "/post/":
+      case "/post":
+        await this.views.post(params.id)
+        break;
+      case "/post/create.html":
+        await this.views.postCreate()
+        break;
+      case "/post/update.html":
+        await this.views.postUpdate(params.id)
+        break;
+      case "/posts":
+      case "/posts/":
+      case "/posts/index.html":
+        await this.views.posts()
+        break;
+      default:
+      // 404 not found
+    }
+  }
+
   static form = {
     handleSubmit(event) {
       event.preventDefault();
@@ -14,11 +56,66 @@ export default class NoroffApp extends NoroffAPI {
   static tags = {
     split(tagString) {
       let tags = tagString.split(",").map((tag) => tag.trim());
-      tags = [...data.tags, ...suggestTags(data.title)].filter(Boolean);
       return tags
     },
     join(tagArray) {
       return tagArray.join(", ")
+    }
+  }
+
+  views = {
+    home: async () => {
+      document.querySelectorAll("[data-auth=logout]").forEach(button => {
+        button.addEventListener("click", () => {
+          this.auth.logout()
+        })
+      })
+    },
+    login: async () => {
+      document.forms.login.addEventListener("submit", this.events.login);
+    },
+    register: async () => {
+      document.forms.register.addEventListener("submit", this.events.register);
+    },
+    post: async (id) => {
+      const post = await this.post.read(id)
+      document.querySelector("input#id").value = id;
+      document.querySelector(".title").innerText = post.title;
+      document.querySelector(".body").innerText = post.body;
+      document.querySelector(".tags").innerText = NoroffApp.tags.join(post.tags);
+      document.querySelector("a.update").href += post.id;
+      document.forms.deletePost.addEventListener("submit", this.events.post.delete)
+    },
+    postUpdate: async (id) => {
+      try {
+        const post = await this.post.read(id);
+        document.querySelector("input#id").value = id;
+        document.querySelector("#title").value = post.title;
+        document.querySelector("#body").value = post.body;
+        document.querySelector("#tags").value = post.tags.join(", ");
+
+        document.forms.updatePost.addEventListener("submit", this.events.post.update)
+      } catch (error) {
+        alert(error);
+      }
+    },
+    postCreate: async () => {
+      document.forms.createPost.addEventListener("submit", this.events.post.create)
+    },
+    posts: async () => {
+      const posts = await this.posts.read();
+      const ul = document.querySelector("ul");
+
+      const listItems = posts.map(post => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+        a.innerText = post.title;
+        a.href = `/post/?id=${post.id}`
+        li.append(a)
+        return li
+      })
+
+      ul.append(...listItems)
     }
   }
 
@@ -27,7 +124,7 @@ export default class NoroffApp extends NoroffAPI {
       const data = NoroffApp.form.handleSubmit(event)
 
       try {
-        await api.auth.login(data)
+        await this.auth.login(data)
         window.location.href = "/"
       } catch (error) {
         alert(error);
@@ -37,7 +134,7 @@ export default class NoroffApp extends NoroffAPI {
       const data = NoroffApp.form.handleSubmit(event)
 
       try {
-        await api.auth.register(data)
+        await this.auth.register(data)
       } catch (error) {
         alert(error);
       }
@@ -48,51 +145,23 @@ export default class NoroffApp extends NoroffAPI {
         data.tags = NoroffApp.tags.split(data.tags)
 
         try {
-          const post = await api.post.create(data)
+          const post = await this.post.create(data)
           window.location.href = `/post/?id=${post.id}`;
         } catch (error) {
           alert(error);
         }
       },
-      update: async () => {
-        const id = currentPostId();
+      update: async (event) => {
+        const { id, ...post } = NoroffApp.form.handleSubmit(event)
+
+        post.tags = post.tags
+          .split(",")
+          .map((tag) => tag.trim())
+          .filter(Boolean);
 
         try {
-          const post = await api.post.read(id);
-
-          document.querySelector("#title").value = post.title;
-          document.querySelector("#body").value = post.body;
-          document.querySelector("#tags").value = post.tags.join(", ");
-
-          document.forms.updatePost.addEventListener("submit", async (event) => {
-            const data = NoroffApp.form.handleSubmit(event)
-
-            data.tags = data.tags
-              .split(",")
-              .map((tag) => tag.trim())
-              .filter(Boolean);
-
-            try {
-              await api.post.update(id, data);
-              window.location.href = `/post/?id=${id}`;
-            } catch (error) {
-              alert(error);
-            }
-          });
-        } catch (error) {
-          alert(error);
-        }
-      },
-      view: async (id) => {
-        try {
-          const post = await this.post.read(id)
-          document.querySelector("input#id").value = id;
-          document.querySelector(".title").innerText = post.title;
-          document.querySelector(".body").innerText = post.body;
-          document.querySelector(".tags").innerText = NoroffApp.tags.join(post.tags);
-          document.querySelector("a.update").href += post.id;
-
-          document.forms.deletePost.addEventListener("submit", this.events.post.delete)
+          await this.post.update(id, post);
+          window.location.href = `/post/?id=${id}`;
         } catch (error) {
           alert(error);
         }
@@ -108,7 +177,6 @@ export default class NoroffApp extends NoroffAPI {
           alert(error);
         }
       }
-
     }
   }
 }
